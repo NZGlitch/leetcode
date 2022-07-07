e63898f98cfbb5accf3f437b1a834c10dcb633bc
Chris Noldus <chris@noldus.geek.nz>
2022-07-08 11:35:44 +1200

    Optimised 20

--- src/nz/noldus/breakfastclub/problems/Question20.java
+++ src/nz/noldus/breakfastclub/problems/Question20.java
@@ -44,23 +44,31 @@ class Question20 {
     //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> parens = new Stack();
-        Map<Character,Character> matching = new HashMap();
-        matching.put('(',')');
-        matching.put('[',']');
-        matching.put('{','}');
 
-        for (int i = 0; i<s.length(); i++) {
-            char next = s.charAt(i);
-            //Opening bracket
-            if (matching.containsKey(next)) {
-                parens.push(next);
-            } else {
-                if (parens.isEmpty()) return false;
-                char close = parens.pop();
-                if (matching.get(close) != next) return false;
-            }
+        // Stacks are ideally suited to this sort of problem, as they will keep a track of the last
+        // opening bracket automatically for us
+        Stack<Character> parens = new Stack();
+
+        //We want a mapping of opening brackets to closing brackets - the most optimal way to do this
+        //in java is to use ascii code as an index on a char array
+        char[] mapParens = new char[126];
+        mapParens['('] = ')'; mapParens['['] = ']'; mapParens['{'] = '}';
+        int idx = -1;
+
+        while (++idx < s.length()) {
+
+            //Get the next character in the string
+            char next = s.charAt(idx);
+
+            // If the next char is in our map (i.e. != \0), it is an opening bracket
+            if (mapParens[next] != '\u0000') parens.push(next);
+            else if ( !parens.isEmpty() ){
+                // Otherwise the character's closing brace MUST be mapped to the current top of the stack
+                if (mapParens[parens.pop()] != next) return false;
+            } else return false;  //Nothing on stack to match
+
         }
+        //End of string without errors, result is true if stack is empty
         return parens.isEmpty();
     }
 }
