0a8ae6c0f9742f62ef036405aa459081faed22d9
Chris Noldus <chris@noldus.geek.nz>
2022-07-29 11:30:24 +1200

    initial solution for q22

---
+++ src/nz/noldus/breakfastclub/problems/Question22.java
@@ -0,0 +1,30 @@
+//Given n pairs of parentheses, write a function to generate all combinations 
+//of well-formed parentheses. 
+//
+// 
+// Example 1: 
+// Input: n = 3
+//Output: ["((()))","(()())","(())()","()(())","()()()"]
+// Example 2: 
+// Input: n = 1
+//Output: ["()"]
+// 
+// 
+// Constraints: 
+//
+// 
+// 1 <= n <= 8 
+// 
+// Related Topics String Dynamic Programming Backtracking 👍 14194 👎 532
+
+
+class Question22 {
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
---
+++ src/nz/noldus/breakfastclub/problems/Question242.java
@@ -0,0 +1,43 @@
+//Given two strings s and t, return true if t is an anagram of s, and false 
+//otherwise. 
+//
+// An Anagram is a word or phrase formed by rearranging the letters of a 
+//different word or phrase, typically using all the original letters exactly once. 
+//
+// 
+// Example 1: 
+// Input: s = "anagram", t = "nagaram"
+//Output: true
+// Example 2: 
+// Input: s = "rat", t = "car"
+//Output: false
+// 
+// 
+// Constraints: 
+//
+// 
+// 1 <= s.length, t.length <= 5 * 10⁴ 
+// s and t consist of lowercase English letters. 
+// 
+//
+// 
+// Follow up: What if the inputs contain Unicode characters? How would you 
+//adapt your solution to such a case? 
+// Related Topics Hash Table String Sorting 👍 6333 👎 234
+
+
+class Question242 {
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int[] ht = new int[255];
+        int zeros = 0;
+        for (int i=0; i< s.length(); i++) ht[s.charAt(i)]++;
+        for (int i=0; i< t.length(); i++) ht[t.charAt(i)]--;
+        for (int i=0; i<255; i++) if (ht[i] != 0) return false;
+        return true;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
