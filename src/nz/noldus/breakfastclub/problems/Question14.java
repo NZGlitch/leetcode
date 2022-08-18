756e9612ffdc994a23855accad84f46bad40c4f2
Chris Noldus <chris@noldus.geek.nz>
2022-08-19 09:13:31 +1200

    Q14 Optimised

--- src/nz/noldus/breakfastclub/problems/Question14.java
+++ src/nz/noldus/breakfastclub/problems/Question14.java
@@ -30,20 +30,39 @@
 // Related Topics String 👍 9692 👎 3293
 
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 class Question14 {
     //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        String best = strs[0];
-        for (int i=0; i<strs.length; i++) {
-            String newBest = "";
-            for(int j=0; j<best.length() && j< strs[i].length(); j++) {
-                if (best.charAt(j) == strs[i].charAt(j)) newBest+=best.charAt(j);
-                else break;
+        // Lets sort by length first - as we only need to look
+        // at the shortest string first
+        Arrays.sort(strs, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o1.length()-o2.length();
             }
-            best = newBest;
+        });
+
+        // The best possible result is the shortest string
+        char[] best = strs[0].toCharArray();
+        int len = best.length;
+
+        //Iterate through the remainin strings
+        for (int i=1; i<strs.length; i++) {
+            // Compare our current best with this string
+            for (int j = 0; j < len; j++) {
+                if (best[j] != strs[i].charAt(j)) {
+                    //Not a match
+                    len = j;
+                    break;
+                }
+            }
         }
-        return best;
+        // Use the len chars from best[] to construct the answer
+        return new String(best, 0, len);
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
