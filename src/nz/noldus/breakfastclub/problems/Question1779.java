476b45dcd6dc60e21a9128dec9c89af74d8d110e
Chris Noldus <chris@noldus.geek.nz>
2022-09-16 09:18:24 +1200

    Q1779

---
+++ src/nz/noldus/breakfastclub/problems/Question1779.java
@@ -0,0 +1,81 @@
+////You are given two integers, x and y, which represent your current location 
+//on 
+////a Cartesian grid: (x, y). You are also given an array points where each 
+//points[
+////i] = [ai, bi] represents that a point exists at (ai, bi). A point is valid 
+//if it 
+////shares the same x-coordinate or the same y-coordinate as your location. 
+////
+//// Return the index (0-indexed) of the valid point with the smallest 
+//Manhattan 
+////distance from your current location. If there are multiple, return the 
+//valid 
+////point with the smallest index. If there are no valid points, return -1. 
+////
+//// The Manhattan distance between two points (x1, y1) and (x2, y2) is abs(x1 -
+// 
+////x2) + abs(y1 - y2). 
+////
+//// 
+//// Example 1: 
+////
+//// 
+////Input: x = 3, y = 4, points = [[1,2],[3,1],[2,4],[2,3],[4,4]]
+////Output: 2
+////Explanation: Of all the points, only [3,1], [2,4] and [4,4] are valid. Of 
+//the 
+////valid points, [2,4] and [4,4] have the smallest Manhattan distance from 
+//your 
+////current location, with a distance of 1. [2,4] has the smallest index, so 
+//return 2. 
+////
+////
+//// Example 2: 
+////
+//// 
+////Input: x = 3, y = 4, points = [[3,4]]
+////Output: 0
+////Explanation: The answer is allowed to be on the same location as your 
+//current 
+////location. 
+////
+//// Example 3: 
+////
+//// 
+////Input: x = 3, y = 4, points = [[2,3]]
+////Output: -1
+////Explanation: There are no valid points. 
+////
+//// 
+//// Constraints: 
+////
+//// 
+//// 1 <= points.length <= 10⁴ 
+//// points[i].length == 2 
+//// 1 <= x, y, ai, bi <= 10⁴ 
+//// 
+//// Related Topics Array 👍 555 👎 107
+//
+
+
+class Question1779 {
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int nearestValidPoint(int x, int y, int[][] points) {
+        int best = -1;
+        int bdist = Integer.MAX_VALUE;
+        for (int i=0; i<points.length; i++) {
+            if (points[i][0] == x || points[i][1] == y) {
+                int mdist = Math.abs(x-points[i][0]) + Math.abs(y-points[i][1]);
+                if (mdist < bdist) {
+                    best = i;
+                    bdist = mdist;
+                }
+            }
+        }
+        return best;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
